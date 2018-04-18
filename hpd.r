03170be30e7a78f0332b4b2af8122116a66a0fac
Stephen A. Collins-Elliott <sce@utk.edu>
2018-04-18 11:58:24 -0400

    Update hpd.r

--- hpd.r
+++ hpd.r
@@ -1,114 +1,240 @@
 library(coda)
 library(ggplot2)
+library(fitdistrplus)
+library(MCMCpack)
+library(fitdistrplus)
 
-CNcoins = read.csv("finalresults-['CN'].csv")
-PTcoins = read.csv("finalresults-['PT'].csv")
-PIcoins = read.csv("finalresults-['PI'].csv")
-MZcoins = read.csv("finalresults-['MZ'].csv")
+#setwd("C:/...") 
+#import bootstrap and subsampling results from python
 
-CNrmean = read.csv("rmeancollected-['CN'].csv")
-PTrmean = read.csv("rmeancollected-['PT'].csv")
-PIrmean = read.csv("rmeancollected-['PI'].csv")
-MZrmean = read.csv("rmeancollected-['MZ'].csv")
+CN.boot = read.csv("rmeancollected-boot-['CN'].csv")
+PT.boot = read.csv("rmeancollected-boot-['PT'].csv")
+PI.boot = read.csv("rmeancollected-boot-['PI'].csv")
+MZ.boot = read.csv("rmeancollected-boot-['MZ'].csv")
+
+CN.sub = read.csv("rmeancollected-subsampled-['CN'].csv")
+PT.sub = read.csv("rmeancollected-subsampled-['PT'].csv")
+PI.sub = read.csv("rmeancollected-subsampled-['PI'].csv")
+MZ.sub = read.csv("rmeancollected-subsampled-['MZ'].csv")
+
+daterange <- MZcoins[,1]
+
+#create bootstrap and subsampling dataframes
+MZ.b = data.frame(daterange)
+MZ.s = data.frame(daterange)
+PI.b = data.frame(daterange)
+PI.s = data.frame(daterange)
+CN.b = data.frame(daterange)
+CN.s = data.frame(daterange)
+PT.b = data.frame(daterange)
+PT.s = data.frame(daterange)
+
+lower.b <- c()
+upper.b <- c()
+lower.s <- c()
+upper.s <- c()
+
+#calculate hpd for both bootstrap and subsampling results
+for (i in 1:dim(MZ.sub)[1]){
+	samples.b <- as.numeric(as.vector( MZ.boot[i,] ))
+	samples.s <- as.numeric(as.vector( MZ.sub[i,] ))
+
+	if(sum(samples.b) > 0){
+	nonzerosamples.b <- samples.b[samples.b > 0]
+	x.b <- nonzerosamples.b
+	den.b <- density(x.b)
+	dat.b <- data.frame(x = den.b$x, y = den.b$y)
+	fit.gamma.b <- fitdist(x.b, "gamma", lower = c(0, 0))
+	shape.b <- fit.gamma.b$estimate[1]
+	rate.b <- fit.gamma.b$estimate[2]
+	res.b <- rgamma(100000, shape = shape.b, rate = rate.b)
+	hpd.b <- HPDinterval(as.mcmc(res.b), prob = 0.5)
+
+	lower.b <- c(lower.b, hpd.b[1])
+	upper.b <- c(upper.b, hpd.b[2]) }
+
+	if(sum(samples.b) == 0){
+	lower.b <- c(lower.b, 0)
+	upper.b <- c(upper.b, 0) 
+	}
+
+	if (sum(samples.s) > 0){
+	nonzerosamples.s <- samples.s[samples.s > 0]
+	x.s <- nonzerosamples.s
+	den.s <- density(x.s)
+	dat.s <- data.frame(x = den.s$x, y = den.s$y)
+	fit.gamma.s <- fitdist(x.s, "gamma", lower = c(0, 0))
+	shape.s <- fit.gamma.s$estimate[1]
+	rate.s <- fit.gamma.s$estimate[2]
+	res.s <- rgamma(100000, shape = shape.s, rate = rate.s)
+	hpd.s <- HPDinterval(as.mcmc(res.s), prob = 0.5)
+
+	lower.s <- c(lower.s, hpd.s[1])
+	upper.s <- c(upper.s, hpd.s[2]) }
 
 
-MZcoinsdata = MZcoins
-colnames(MZcoinsdata) <- c("year","mu", "musub", "nu", "xi")
-as.data.frame.matrix(MZcoinsdata) 
-x <- seq(0, 2.5, length = 1000)
-lower <- c()
-upper <- c()
-for (i in 1:dim(MZcoins)[1]){
-	pdf1 <- rgamma(x, shape  = MZcoinsdata[i,]$nu, scale = MZcoinsdata[i,]$xi)
-	z <- HPDinterval(as.mcmc(pdf1), prob=0.5)
-	lower <- c(lower, z[1])
-	upper <- c(upper, z[2]) 
+	if(sum(samples.s) == 0){
+	lower.s <- c(lower.s, 0)
+	upper.s <- c(upper.s, 0) 
+	}
 }
-MZcoinsdata["upper"] <- upper
-MZcoinsdata["lower"] <- lower
+MZ.b["upper"] <- upper.b
+MZ.b["lower"] <- lower.b
+MZ.s["upper"] <- upper.s
+MZ.s["lower"] <- lower.s
+lower.b <- c()
+upper.b <- c()
+lower.s <- c()
+upper.s <- c()
 
-CNcoinsdata = CNcoins
-colnames(CNcoinsdata) <- c("year","mu", "musub", "nu", "xi")
-as.data.frame.matrix(CNcoinsdata) 
-x <- seq(0, 2.5, length = 1000)
-lower <- c()
-upper <- c()
-for (i in 1:dim(CNcoins)[1]){
-	pdf1 <- rgamma(x, shape  = CNcoinsdata[i,]$nu, scale = CNcoinsdata[i,]$xi)
-	z <- HPDinterval(as.mcmc(pdf1), prob=0.5)
-	lower <- c(lower, z[1])
-	upper <- c(upper, z[2]) 
+for (i in 1:dim(PT.sub)[1]){
+	samples.b <- as.numeric(as.vector( PT.boot[i,] ))
+	samples.s <- as.numeric(as.vector( PT.sub[i,] ))
+
+	if(sum(samples.b) > 0){
+	nonzerosamples.b <- samples.b[samples.b > 0]
+	x.b <- nonzerosamples.b
+	den.b <- density(x.b)
+	dat.b <- data.frame(x = den.b$x, y = den.b$y)
+	fit.gamma.b <- fitdist(x.b, "gamma", lower = c(0, 0))
+	shape.b <- fit.gamma.b$estimate[1]
+	rate.b <- fit.gamma.b$estimate[2]
+	res.b <- rgamma(100000, shape = shape.b, rate = rate.b)
+	hpd.b <- HPDinterval(as.mcmc(res.b), prob = 0.5)
+
+	lower.b <- c(lower.b, hpd.b[1])
+	upper.b <- c(upper.b, hpd.b[2]) }
+
+	if(sum(samples.b) == 0){
+	lower.b <- c(lower.b, 0)
+	upper.b <- c(upper.b, 0) 
+	}
+
+	if (sum(samples.s) > 0){
+	nonzerosamples.s <- samples.s[samples.s > 0]
+	x.s <- nonzerosamples.s
+	den.s <- density(x.s)
+	dat.s <- data.frame(x = den.s$x, y = den.s$y)
+	fit.gamma.s <- fitdist(x.s, "gamma", lower = c(0, 0))
+	shape.s <- fit.gamma.s$estimate[1]
+	rate.s <- fit.gamma.s$estimate[2]
+	res.s <- rgamma(100000, shape = shape.s, rate = rate.s)
+	hpd.s <- HPDinterval(as.mcmc(res.s), prob = 0.5)
+
+	lower.s <- c(lower.s, hpd.s[1])
+	upper.s <- c(upper.s, hpd.s[2]) }
+
+	if(sum(samples.s) == 0){
+	lower.s <- c(lower.s, 0)
+	upper.s <- c(upper.s, 0) 
+	}
 }
-CNcoinsdata["upper"] <- upper
-CNcoinsdata["lower"] <- lower
+PT.b["upper"] <- upper.b
+PT.b["lower"] <- lower.b
+PT.s["upper"] <- upper.s
+PT.s["lower"] <- lower.s
+lower.b <- c()
+upper.b <- c()
+lower.s <- c()
+upper.s <- c()
 
-PIcoinsdata = PIcoins
-colnames(PIcoinsdata) <- c("year","mu", "musub", "nu", "xi")
-as.data.frame.matrix(PIcoinsdata) 
-x <- seq(0, 2.5, length = 1000)
-lower <- c()
-upper <- c()
-for (i in 1:dim(CNcoins)[1]){
-	pdf1 <- rgamma(x, shape  = PIcoinsdata[i,]$nu, scale = PIcoinsdata[i,]$xi)
-	z <- HPDinterval(as.mcmc(pdf1), prob=0.5)
-	lower <- c(lower, z[1])
-	upper <- c(upper, z[2]) 
+for (i in 1:dim(PI.sub)[1]){
+	samples.b <- as.numeric(as.vector( PI.boot[i,] ))
+	samples.s <- as.numeric(as.vector( PI.sub[i,] ))
+
+	if(sum(samples.b) > 0){
+	nonzerosamples.b <- samples.b[samples.b > 0]
+	x.b <- nonzerosamples.b
+	den.b <- density(x.b)
+	dat.b <- data.frame(x = den.b$x, y = den.b$y)
+	fit.gamma.b <- fitdist(x.b, "gamma", lower = c(0, 0))
+	shape.b <- fit.gamma.b$estimate[1]
+	rate.b <- fit.gamma.b$estimate[2]
+	res.b <- rgamma(100000, shape = shape.b, rate = rate.b)
+	hpd.b <- HPDinterval(as.mcmc(res.b), prob = 0.5)
+
+	lower.b <- c(lower.b, hpd.b[1])
+	upper.b <- c(upper.b, hpd.b[2]) }
+
+	if(sum(samples.b) == 0){
+	lower.b <- c(lower.b, 0)
+	upper.b <- c(upper.b, 0) 
+	}
+
+	if (sum(samples.s) > 0){
+	nonzerosamples.s <- samples.s[samples.s > 0]
+	x.s <- nonzerosamples.s
+	den.s <- density(x.s)
+	dat.s <- data.frame(x = den.s$x, y = den.s$y)
+	fit.gamma.s <- fitdist(x.s, "gamma", lower = c(0, 0))
+	shape.s <- fit.gamma.s$estimate[1]
+	rate.s <- fit.gamma.s$estimate[2]
+	res.s <- rgamma(100000, shape = shape.s, rate = rate.s)
+	hpd.s <- HPDinterval(as.mcmc(res.s), prob = 0.5)
+
+	lower.s <- c(lower.s, hpd.s[1])
+	upper.s <- c(upper.s, hpd.s[2]) }
+
+
+	if(sum(samples.s) == 0){
+	lower.s <- c(lower.s, 0)
+	upper.s <- c(upper.s, 0) 
+	}
 }
-PIcoinsdata["upper"] <- upper
-PIcoinsdata["lower"] <- lower
+PI.b["upper"] <- upper.b
+PI.b["lower"] <- lower.b
+PI.s["upper"] <- upper.s
+PI.s["lower"] <- lower.s
+lower.b <- c()
+upper.b <- c()
+lower.s <- c()
+upper.s <- c()
+
+for (i in 1:dim(CN.sub)[1]){
+	samples.b <- as.numeric(as.vector( CN.boot[i,] ))
+	samples.s <- as.numeric(as.vector( CN.sub[i,] ))
+
+	if(sum(samples.b) > 0){
+	nonzerosamples.b <- samples.b[samples.b > 0]
+	x.b <- nonzerosamples.b
+	den.b <- density(x.b)
+	dat.b <- data.frame(x = den.b$x, y = den.b$y)
+	fit.gamma.b <- fitdist(x.b, "gamma", lower = c(0, 0))
+	shape.b <- fit.gamma.b$estimate[1]
+	rate.b <- fit.gamma.b$estimate[2]
+	res.b <- rgamma(100000, shape = shape.b, rate = rate.b)
+	hpd.b <- HPDinterval(as.mcmc(res.b), prob = 0.5)
+
+	lower.b <- c(lower.b, hpd.b[1])
+	upper.b <- c(upper.b, hpd.b[2]) }
+
+	if(sum(samples.b) == 0){
+	lower.b <- c(lower.b, 0)
+	upper.b <- c(upper.b, 0) 
+	}
+
+	if (sum(samples.s) > 0){
+	nonzerosamples.s <- samples.s[samples.s > 0]
+	x.s <- nonzerosamples.s
+	den.s <- density(x.s)
+	dat.s <- data.frame(x = den.s$x, y = den.s$y)
+	fit.gamma.s <- fitdist(x.s, "gamma", lower = c(0, 0))
+	shape.s <- fit.gamma.s$estimate[1]
+	rate.s <- fit.gamma.s$estimate[2]
+	res.s <- rgamma(100000, shape = shape.s, rate = rate.s)
+	hpd.s <- HPDinterval(as.mcmc(res.s), prob = 0.5)
+
+	lower.s <- c(lower.s, hpd.s[1])
+	upper.s <- c(upper.s, hpd.s[2]) }
 
 
-PTcoinsdata = PTcoins
-colnames(PTcoinsdata) <- c("year","mu", "musub", "nu", "xi")
-as.data.frame.matrix(PTcoinsdata) 
-x <- seq(0, 2.5, length = 1000)
-lower <- c()
-upper <- c()
-for (i in 1:dim(PTcoins)[1]){
-	pdf1 <- rgamma(x, shape  = PTcoinsdata[i,]$nu, scale = PTcoinsdata[i,]$xi)
-	z <- HPDinterval(as.mcmc(pdf1), prob=0.5)
-	lower <- c(lower, z[1])
-	upper <- c(upper, z[2]) 
+	if(sum(samples.s) == 0){
+	lower.s <- c(lower.s, 0)
+	upper.s <- c(upper.s, 0) 
+	}
 }
-PTcoinsdata["upper"] <- upper
-PTcoinsdata["lower"] <- lower
-
-
-ggplot(data=MZcoinsdata, aes(year)) +  
-geom_ribbon(aes(ymin=MZcoinsdata$lower,ymax=MZcoinsdata$upper), fill="#000000", alpha="0.10") +
-geom_ribbon(aes(ymin=CNcoinsdata$lower,ymax=CNcoinsdata$upper), fill="#CC0000", alpha="0.10") +
-geom_ribbon(aes(ymin=PIcoinsdata$lower,ymax=PIcoinsdata$upper), fill="#0000FF", alpha="0.10") +
-geom_ribbon(aes(ymin=PTcoinsdata$lower,ymax=PTcoinsdata$upper), fill="#009933", alpha="0.10") +
-  theme_classic()
-
-
-
-z <- 0
-for (i in 1:dim(MZcoinsdata)[1]){
-zz <- MZcoinsdata$mu[i]
-if (zz > 0) { z <- z + 1 }
-}
-MZbias <- (1 / z) * sum(MZcoinsdata$mu - MZcoinsdata$musub)
-
-z <- 0
-for (i in 1:dim(CNcoinsdata)[1]){
-zz <- CNcoinsdata$mu[i]
-if (zz > 0) { z <- z + 1 }
-}
-CNbias <- (1 / z) * sum(CNcoinsdata$mu - CNcoinsdata$musub)
-
-z <- 0
-for (i in 1:dim(PTcoinsdata)[1]){
-zz <- PTcoinsdata$mu[i]
-if (zz > 0) { z <- z + 1 }
-}
-PTbias <- (1 / z) * sum(PTcoinsdata$mu - PTcoinsdata$musub)
-
-z <- 0
-for (i in 1:dim(PIcoinsdata)[1]){
-zz <- PIcoinsdata$mu[i]
-if (zz > 0) { z <- z + 1 }
-}
-PIbias <- (1 / z) * sum(PIcoinsdata$mu - PIcoinsdata$musub)
-
+CN.b["upper"] <- upper.b
+CN.b["lower"] <- lower.b
+CN.s["upper"] <- upper.s
+CN.s["lower"] <- lower.s
 
